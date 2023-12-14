24164a382c6f4f80968eac8e11528270f6a656f4
poiz1245 <wva960@gmail.com>
2023-12-14 20:47:28 +0900

    Upload

---
+++ Assets/Scripts/ObjData.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ObjData : ScriptableObject
+{
+    [System.Serializable]
+    public class Attribute
+    {
+        public string productName;
+        public string productCategory;
+        public string productMaterial;
+        public string Floor;
+        public string objectSize;
+        public int workFlow;
+        public string startDate;
+        public string endDate;
+        public string currentStatus;
+    }
+
+    public List<Attribute> list = new List<Attribute>();
+}
